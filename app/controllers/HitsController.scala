e0cd23bcf20308a8dea0b77ed699657407e23e02
pmonteiro <paulo.from.portugal@gmail.com>
2020-11-19 14:18:38 +0000

    Add DELETE API resource

--- app/controllers/HitsController.scala
+++ app/controllers/HitsController.scala
@@ -1,20 +1,22 @@
 package controllers
 
+//import akka.http.scaladsl.model.{HttpEntity, HttpRequest, HttpResponse}
+
 import famousHits.FamousHits
 import javax.inject.{Inject, Singleton}
 import play.api.libs.json.Json
 import play.api.libs.json.Json._
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import play.modules.reactivemongo._
-import reactivemongo.play.json._
-import reactivemongo.play.json.collection.{JSONCollection, _}
+import reactivemongo.play.json.collection.JSONCollection
 
 import scala.concurrent.{ExecutionContext, Future}
+import reactivemongo.play.json._, collection._
 
 @Singleton
-class HitsController @Inject()(controllerComponents: ControllerComponents,
+class HitsController @Inject()( controllerComponents: ControllerComponents,
                                val reactiveMongoApi: ReactiveMongoApi)
-  extends AbstractController(controllerComponents) with MongoController with ReactiveMongoComponents {
+  extends AbstractController(controllerComponents ) with MongoController with ReactiveMongoComponents {
   implicit def ec: ExecutionContext = controllerComponents.executionContext
 
   def getHits: Action[AnyContent] = Action {
@@ -27,18 +29,28 @@ class HitsController @Inject()(controllerComponents: ControllerComponents,
     Ok(toJson(hit))
   }
 
+  def deleteHit(id: String): Action[AnyContent] = Action.async {
+    val query = Json.obj(
+      "name" -> id,
+    )
+    collection.flatMap(_.delete.one(query)).map(lastError =>
+      Ok("Mongo LastError: %s".format(lastError)))
+  }
+
+  def collection: Future[JSONCollection] =
+    database.map(_.collection[JSONCollection]("hits"))
+
+
+
   def addHit() = Action.async {
     val json = Json.obj(
       "name" -> "testUser",
       "age" -> "30",
       "created" -> new java.util.Date().getTime()
     )
-    // collection.
-    collection.flatMap(_.insert.one(json)).map(_ => Ok(json))
-  }
 
-  def collection: Future[JSONCollection] =
-    database.map(_.collection[JSONCollection]("hits"))
-}
+    collection.flatMap(_.insert.one(json))map(lastError =>
+      Ok("Mongo LastError: %s".format(lastError)))
+  }}
 
 
