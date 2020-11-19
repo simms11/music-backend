ba51067e16e43693af5e658a2d60a14c5c6c1705
pmonteiro <paulo.from.portugal@gmail.com>
2020-11-19 15:14:48 +0000

    Bind /POST hits payload to Hit object

--- app/controllers/HitsController.scala
+++ app/controllers/HitsController.scala
@@ -1,22 +1,23 @@
 package controllers
 
-//import akka.http.scaladsl.model.{HttpEntity, HttpRequest, HttpResponse}
-
 import famousHits.FamousHits
 import javax.inject.{Inject, Singleton}
+import models.Hit
+import models.Hit.format
+import play.api.data.Form
 import play.api.libs.json.Json
 import play.api.libs.json.Json._
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import play.modules.reactivemongo._
-import reactivemongo.play.json.collection.JSONCollection
+import reactivemongo.play.json._
+import reactivemongo.play.json.collection.{JSONCollection, _}
 
 import scala.concurrent.{ExecutionContext, Future}
-import reactivemongo.play.json._, collection._
 
 @Singleton
-class HitsController @Inject()( controllerComponents: ControllerComponents,
+class HitsController @Inject()(controllerComponents: ControllerComponents,
                                val reactiveMongoApi: ReactiveMongoApi)
-  extends AbstractController(controllerComponents ) with MongoController with ReactiveMongoComponents {
+  extends AbstractController(controllerComponents) with MongoController with ReactiveMongoComponents {
   implicit def ec: ExecutionContext = controllerComponents.executionContext
 
   def getHits: Action[AnyContent] = Action {
@@ -41,16 +42,16 @@ class HitsController @Inject()( controllerComponents: ControllerComponents,
     database.map(_.collection[JSONCollection]("hits"))
 
 
-
-  def addHit() = Action.async {
-    val json = Json.obj(
-      "name" -> "testUser",
-      "age" -> "30",
-      "created" -> new java.util.Date().getTime()
-    )
-
-    collection.flatMap(_.insert.one(json))map(lastError =>
-      Ok("Mongo LastError: %s".format(lastError)))
-  }}
+  def addHit() = Action.async { implicit request =>
+    Hit.form
+      .bindFromRequest()
+      .fold(
+        (formWithErrors: Form[Hit]) => Future.successful(BadRequest(s"Something went wrong => [$formWithErrors]")),
+        hit => {
+          collection.flatMap(_.insert.one(hit)).map(lastError =>
+            Ok("Mongo LastError: %s".format(lastError)))
+        })
+  }
+}
 
 
--- app/models/Hit.scala
+++ app/models/Hit.scala
@@ -1,9 +1,25 @@
 package models
 
-import play.api.libs.json.{Format, Json}
+import play.api.data.Form
+import play.api.data.Forms.{number, text}
+import play.api.data.validation.Constraints
+import play.api.data.validation.Constraints.nonEmpty
+import play.api.data.Forms._
+import play.api.libs.json.{Json, OFormat}
 
 case class Hit(id: Int, name: String, yearReleased: Int, album: String, chartedUSA: Int, fact: String)
 
 object Hit {
-  implicit val format: Format[Hit] = Json.format[Hit]
+  implicit val format: OFormat[Hit] = Json.format[Hit]
+
+  val form: Form[Hit] = Form(
+    mapping(
+    "id" -> number.verifying(Constraints.min(0)),
+    "name" -> text.verifying(nonEmpty),
+    "yearReleased" -> number.verifying(Constraints.min(0)),
+    "album" -> text.verifying(nonEmpty),
+    "chartedUSA" -> number.verifying(Constraints.min(0)),
+    "fact" -> text.verifying(nonEmpty)
+  )(Hit.apply)(Hit.unapply))
+
 }
